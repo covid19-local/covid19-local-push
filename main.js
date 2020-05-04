004ad962ff28d072c20beb6a5d06b56df5919194
Jeff Muller <jeffmuller@muller-media.net>
2020-05-03 22:30:34 -0400

    Implemented city report push

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const COVID_BASE_API = 'https://covid-api.com/api';
 const REGIONS_BASE_API = `${COVID_BASE_API}/regions`;
 const STATES_BASE_API = `${COVID_BASE_API}/provinces`;
 const REPORTS_BASE_API = `${COVID_BASE_API}/reports`;
+const MAX_MESSAGE_BATCH_COUNT = 500;
 let messagingInitialized = false;
 let messagingCredentials = '';
+let sendMessagesInBatches = true;
 
 async function getRegions() {
     let url = REGIONS_BASE_API;
@@ -29,12 +31,23 @@ async function getStateReport(date, regionName, regionProvince) {
     let url = `${REPORTS_BASE_API}?date=${formattedDate}&region_name=${encodeURI(regionName)}&region_province=${encodeURI(regionProvince)}`;
 
     let result = JSON.parse(await request.get(url));
-    console.log('Received state reports from covid api');
+    console.log(`Received state reports for ${regionProvince}, ${regionName} from covid api`);
     return result.data;
 }
 
+function generateTopic(arguments) {
+    let topic = '';
+    for (const argument of arguments) {
+        topic += `${encodeURI(argument).replace("'", '').replace('(', '').replace(')', '')}_`;
+    }
+    if (topic.length > 0) {
+        topic = topic.substring(0, topic.lastIndexOf('_'));
+    }
+    return topic;
+}
+
 function generateStateMessage(stateReport) {
-    let topic = `${encodeURI(stateReport.region.name)}_${encodeURI(stateReport.region.province)}`;
+    let topic = generateTopic([stateReport.region.name, stateReport.region.province]);
     return {
         notification: {
             title: `Latest COVID-19 Cases for ${stateReport.region.province}`,
@@ -43,6 +56,23 @@ function generateStateMessage(stateReport) {
         data: {
             name: stateReport.region.name,
             province: stateReport.region.province,
+            date: stateReport.date
+        },
+        topic: topic
+    };
+}
+
+function generateCityMessage(stateReport, cityReport) {
+    let topic = generateTopic([stateReport.region.name, stateReport.region.province, cityReport.name]);
+    return {
+        notification: {
+            title: `Latest COVID-19 Cases for ${cityReport.name}, ${stateReport.region.province}`,
+            body: `${cityReport.name}, ${stateReport.region.province} has ${cityReport.confirmed} confirmed cases of COVID-19 as of ${cityReport.date}, which is a difference of ${cityReport.confirmed_diff} from the previous day.`
+        },
+        data: {
+            name: stateReport.region.name,
+            province: stateReport.region.province,
+            city: cityReport.name,
             date: stateReport.date
         },
         topic: topic
@@ -58,18 +88,62 @@ function initializeMessaging() {
     }
 }
 
+async function sendMessageBatch(messageBatch) {
+    initializeMessaging();
+    // Send messages to devices subscribed to the provided topic.
+    response = await admin.messaging().sendAll(messageBatch)
+    // Response is the success count
+    return response;
+}
+
+async function sendMessage(message) {
+    initializeMessaging();
+    // Send message to devices subscribed to the provided topic.
+    response = await admin.messaging().send(message)
+    // Response is the id
+    return response;
+}
+
 async function sendMessages(messages) {
-    try {
-        initializeMessaging();
-        console.log('Sending messages...');
-        // Send messages to devices subscribed to the provided topic.
-        response = await admin.messaging().sendAll(messages)
-        // Response is the success count
-        console.log('Successfully sent messages:', response);
-        return response;
-    } catch (error) {
-        console.log('Error sending messages:', error);
+    responses = [];
+    if (sendMessagesInBatches) {
+        messageBatches = [];
+        if (messages <= MAX_MESSAGE_BATCH_COUNT) {
+            messageBatches.push(messages);
+        } else {
+            let batchCount = Math.ceil(messages.length / MAX_MESSAGE_BATCH_COUNT);
+            for (let index = 0; index < batchCount; index++) {
+                let startIndex = index * MAX_MESSAGE_BATCH_COUNT;
+                let endIndex = startIndex + MAX_MESSAGE_BATCH_COUNT;
+                if (endIndex > messages.length) {
+                    endIndex = messages.length;
+                }
+                messageBatches.push(messages.slice(startIndex, endIndex)); 
+            }
+        }
+        for (const messageBatch of messageBatches){ 
+            try {
+                console.log('Sending messages...');
+                response = await sendMessageBatch(messageBatch);
+                console.log('Successfully sent messages:', response);
+                responses.push(response);
+            } catch (error) {
+                console.log('Error sending messages:', error);
+            }
+        }
+    } else {
+        for (const message of messages) {
+            try {
+                console.log('Sending message...');
+                response = await sendMessage(message);
+                console.log('Successfully sent message:', response);
+                responses.push(response);
+            } catch (error) {
+                console.log('Error sending messages:', error);
+            }
+        }
     }
+    return responses;
 }
 
 async function main(args) {
@@ -97,11 +171,15 @@ async function main(args) {
         for (const state of states) {
             let regionProvince = state.province;
             let result = await getStateReport(date, regionName, regionProvince);
-            console.log('Received result from covid api');
             if (result.length > 0) {
                 let stateReport = result[0];
                 let message = generateStateMessage(stateReport);
                 messages.push(message);
+
+                for (const cityReport of stateReport.region.cities) {
+                    message = generateCityMessage(stateReport, cityReport);
+                    messages.push(message);
+                }
             }
         }
         if (messages.length > 0) {
